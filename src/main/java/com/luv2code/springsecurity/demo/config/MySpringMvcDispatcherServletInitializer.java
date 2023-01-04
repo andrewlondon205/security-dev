89d012bd0a68cd172c7c4d923c60cec01bd6c491
Ramai Alejandria <vyasadev931@gmail.com>
2023-01-04 17:35:07 +0100

    security implementations

---
+++ src/main/java/com/luv2code/springsecurity/demo/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,22 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {DemoAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
