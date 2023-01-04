ba055bebf7993010068208f7216bb1188ee7ed09
Ramai Alejandria <vyasadev931@gmail.com>
2023-01-04 20:08:58 +0100

    custom login form implementation

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.*;
@@ -17,5 +18,16 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
                 .withUser(users.username("mary").password("test123").roles("MANAGER"))
                 .withUser(users.username("susan").password("test123").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll();
     }
 }
